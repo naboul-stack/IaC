2f152e99f13a6caa9b4dbcd3f4c2e3e16b567c82
naboul-stack <63788034+naboul-stack@users.noreply.github.com>
2020-05-11 13:55:40 +0200

    ARM Template Storage account

---
+++ modules.ps1
@@ -0,0 +1,7 @@
+$GitBasePath = '<repo clone path>\ProvisionAzureResources'
+$GitBasePath = 
+
+# Deploy Simple Template creating a storage account 
+New-AzResourceGroupDeployment -ResourceGroupName iac-build-rg `
+    -TemplateFile   
+    -TemplateParamterFile 
